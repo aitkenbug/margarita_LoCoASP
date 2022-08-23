9d5c17ecef8364da63f553dd451b74b8cf89fab1
Benjamin Santelices <benjasantelices@hotmail.com>
2022-08-23 16:31:58 -0400

    Implementación de hardware de SPI para el ADC

--- Dev/Full_v5.3_V0R1_1506/Full_v5.3_V0R1_1506.ino
+++ Dev/Full_v5.3_V0R1_1506/Full_v5.3_V0R1_1506.ino
@@ -28,7 +28,7 @@ TinyGPSPlus gps; // Objeto GPS
 SoftwareSerial ss(3, 2); // Conexion serial para conectarse al GPS
 
 
-#define SELPIN 4 //Pin de activación del ADC
+#define CS_ADC 4 //Pin de activación del ADC
 #define DATAOUT 11 // MOSI
 #define DATAIN 12 // MISO
 #define SPICLOCK 13 // CLK
@@ -319,35 +319,16 @@ String BMP()
 }
 //-------------------------Lectura de datos desde ADC------------------------
 
-int read_adc(int channel) {
-  int adcvalue = 0;
-  byte commandbits = B11000000; //command bits - start, mode, chn (3), dont care (3)
+int read_ADC(int channel) {
 
-  //allow channel selection
-  commandbits |= ((channel - 1) << 3);
+  int adcValue = 0;
+  const int byte8 = 0x06; //setup byte
+  int byte16 = (channel - 1) << 14; //bitshifted channel for second block.
 
-  digitalWrite(SELPIN, LOW); //Select adc
-  // setup bits to be written
-  for (int i = 7; i >= 3; i--) {
-    digitalWrite(DATAOUT, commandbits & 1 << i);
-    //cycle clock
-    digitalWrite(SPICLOCK, HIGH);
-    digitalWrite(SPICLOCK, LOW);
-  }
+  digitalWrite(CS_ADC, LOW); //select MCP3204
+  SPI.transfer(byte8);
+  adcValue = SPI.transfer16(byte16) & 0x0FFF; //ADC sample bitmasking.
 
-  digitalWrite(SPICLOCK, HIGH);   //ignores 2 null bits
-  digitalWrite(SPICLOCK, LOW);
-  digitalWrite(SPICLOCK, HIGH);
-  digitalWrite(SPICLOCK, LOW);
-
-  //read bits from adc
-  for (int i = 11; i >= 0; i--) {
-    adcvalue += digitalRead(DATAIN) << i;
-    //cycle clock
-    digitalWrite(SPICLOCK, HIGH);
-    digitalWrite(SPICLOCK, LOW);
-  }
-  digitalWrite(SELPIN, HIGH); //turn off device
-  return adcvalue;
+  digitalWrite(CS_ADC, HIGH); //turn off device
+  return adcValue;
 }
-
