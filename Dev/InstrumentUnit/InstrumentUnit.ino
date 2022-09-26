32a3ee270ee0f601ad58857d342b2f3ef728f8fc
matvidal <matias.vidal.v@gmail.com>
2022-09-26 16:19:12 -0300

    Changed the baudrate and improved the code.

--- Dev/InstrumentUnit/InstrumentUnit.ino
+++ Dev/InstrumentUnit/InstrumentUnit.ino
@@ -6,11 +6,11 @@
 //  /_/  /_/\__,_/_/   \__, /\__,_/_/  /_/\__/\__,_/     /_____/\____/\____/\____/_/  |_/____/_/ |_|  
 //                    /____/                                                                          
 // ------------------------------------------------------------------------------------------------------------------
-// Fifth Generation Low-Cost Automatic Sun Photometer for scale-oriented measurements of Aerosol Optical Depth (AOD)
+// Second Generation Low-Cost Automatic Sun Photometer for scale-oriented measurements of Aerosol Optical Depth (AOD)
 // Space and Planetary Exploration Laboratory, Faculty of Physical and Matemathical Sciences, University of Chile
 // ------------------------------------------------------------------------------------------------------------------
 // First generation developement by Cristobal Garrido and MCI Electronics
-// Second generation developement by Benjamín Santelices, Vicente Aitken, José Ferrada, and Matías VIdal
+// Second generation developement by Benjamín Santelices, Vicente Aitken, and José Ferrada
 // ------------------------------------------------------------------------------------------------------------------
 // INSTRUMENT.INO > Photometer Instrument Unit
 // Firmware for Instrument Unit - Arduino Uno
@@ -37,195 +37,176 @@ SoftwareSerial ss(3, 2); // Conexion serial para conectarse al GPS
 #define CS_SD 8  //SD chip select. Matches hardware SPI bus implementation on 328P.
 
 
-void setup()
-{
-  String zz, zz2, zz3, zz4;
-  pinMode(A2,INPUT); //stop trigger from Tracker Unit init
-  //debug UART, GPS softUART, BMP init
-  Serial.begin(9600); 
-  ss.begin(GPSBaud); 
-  pressure.begin(); 
+void setup() {
+    String zz, zz2, zz3, zz4;
+    pinMode(A2,INPUT); //stop trigger from Tracker Unit init
+    //debug UART, GPS softUART, BMP init
+    Serial.begin(115200); 
+    ss.begin(GPSBaud); 
+    pressure.begin(); 
 
- //---MEASURING STARTS HERE---
-  zz = ID;
-  while (digitalRead(A2)== 0)
-  {
-    //do nothing while A2 is low
-    //WARN: isn't A2 being checked twice? here and on data() function. possible efficiency boost here.
-  }
-  
-  delay(100);
-  zz2 = data(); //ADC data
+    //---MEASURING STARTS HERE---
+    zz = ID;
+    Serial.println("Init");
+    while (digitalRead(A2)== 0) {
+        //do nothing while A2 is low
+        //WARN: isn't A2 being checked twice? here and on data() function. possible efficiency boost here.
+    }
 
-  delay(100);
-  zz3 = GPS(); //GPS data
-  delay(100);
+    delay(100);
+    zz2 = data(); //ADC data
 
-  SD.begin(CS_SD); //SD init
-  delay(100);
-  
-  zz4 = BMP(); //BMP180 data
-  delay(100);
+    delay(100);
+    zz3 = GPS(); //GPS data
+    delay(100);
 
-  //---DATA STORAGE---
-  File dataFile = SD.open("Data.txt", FILE_WRITE);
-  if (dataFile) { //check availability
-    dataFile.print(zz);
-    dataFile.print(zz2);
-    dataFile.print(zz3);
-    dataFile.println(zz4); //WARN: maybe better to assemble string internally? not really relevant tho.
-    dataFile.close();
-  }
-  else {
-    //Serial.println(F("Error en Data.txt")); //WARN: good idea to implement some sort of debugging here.
-  }
-  delay(100);
+    SD.begin(CS_SD); //SD init
+    delay(100);
+
+    zz4 = BMP(); //BMP180 data
+    delay(100);
+
+    //---DATA STORAGE---
+    File dataFile = SD.open("Data.txt", FILE_WRITE);
+    if (dataFile) { //check availability
+        dataFile.print(zz);
+        dataFile.print(zz2);
+        dataFile.print(zz3);
+        dataFile.println(zz4); //WARN: maybe better to assemble string internally? not really relevant tho.
+        dataFile.close();
+    }
+    else {
+        //Serial.println(F("Error en Data.txt")); //WARN: good idea to implement some sort of debugging here.
+    }
+    delay(100);
 }
 
-void loop() //nothing happens here.
-{
+void loop() {//nothing happens here.
 }
 
 //---DATA ACQUISITION FUNCTIONS---
 
-String data() //Sensor data processing and collation.
-{
-  const char coma = ','; //WARN: is this necessary?
-  int readvalue;
-  int data1, data2, data3, data4;
-  char zz[30];
-  unsigned long tiempo;
-  tiempo = millis(); //WARN: why?
-  data1 = 0;
-  data2 = 0;
-  data3 = 0;
-  data4 = 0;
-  //Sensor readout, keep highest value of each sensor.
-  while (digitalRead(A2)) //Second check of A2 (?)
-  {
-    readvalue = read_ADC(1);
-
-    if (data1 <= readvalue)
-    {
-      data1 = readvalue;
-    }
-
-    readvalue = read_ADC(2);
-
-    if (data2 <= readvalue)
-    {
-      data2 = readvalue;
-    }
-    readvalue = read_ADC(3);
-
-    if (data3 <= readvalue)
-    {
-      data3 = readvalue;
-    }
-
-    readvalue = read_ADC(4);
-
-    if (data4 <= readvalue)
-    {
-      data4 = readvalue;
-    }
-  }
-  sprintf(zz, ",%s,%s,%s,%s", data1, data2, data3, data4);
-  Serial.print("Sensor data: ");
-  Serial.println(zz);
-  //WARN: maybe some debugging here?
-  return zz;
-}
-
-String GPS() //GPS data parsing and collation, hugely inneficient. To be replaced by straight NMEA communication.
-{
-  char gps_data[] = "Invalid data";
-  unsigned long tiempo = millis(); //El tiempo de inicio para marcar
-
-  while (millis() < tiempo + 30000) {
-    while (ss.available() > 0) {
-      if (gps.encode(ss.read())) {
-        if (gps.location.isValid()) {
-	  // isValid checks for the complete GPRMC frame.
-	  sprintf(gps_data, ",%s,%s,%s,%s,%s,%s,%s,%s", gps.location.lat(),
-                                                        gps.location.lng(),
-                                                        gps.date.day(),
-                                                        gps.date.month(),
-                                                        gps.date.year(),
-                                                        gps.time.hour(),
-                                                        gps.time.minute(),
-                                                        gps.time.second());
-	  break;
+String data() {
+    //Sensor data processing and collation.
+    const char coma = ','; //WARN: is this necessary?
+    int readvalue;
+    int data1, data2, data3, data4;
+    char zz[30];
+    unsigned long tiempo;
+    tiempo = millis(); //WARN: why?
+    data1 = 0;
+    data2 = 0;
+    data3 = 0;
+    data4 = 0;
+    //Sensor readout, keep highest value of each sensor.
+    while (digitalRead(A2)) {//Second check of A2 (?)
+        readvalue = read_ADC(1);
+        if (data1 <= readvalue) {
+            data1 = readvalue;
         }
-      }
+
+        readvalue = read_ADC(2);
+        if (data2 <= readvalue) {
+            data2 = readvalue;
+        }
+
+        readvalue = read_ADC(3);
+        if (data3 <= readvalue) {
+            data3 = readvalue;
+        }
+
+        readvalue = read_ADC(4);
+        if (data4 <= readvalue) {
+            data4 = readvalue;
+        }
     }
-  }
-  Serial.print("GPS data: ");
-  Serial.println(gps_data);
-  return gps_data;
+    sprintf(zz, ",%s,%s,%s,%s", data1, data2, data3, data4);
+    Serial.print("Sensor data: ");
+    Serial.println(zz);
+    //WARN: maybe some debugging here?
+    return zz;
+}
+
+String GPS() {
+    //GPS data parsing and collation, hugely inneficient. To be replaced by straight NMEA communication.
+    char gps_data[] = "Invalid data";
+    unsigned long tiempo = millis(); //El tiempo de inicio para marcar
+
+    while (millis() < tiempo + 30000) {
+        while (ss.available() > 0) {
+            if (gps.encode(ss.read())) {
+                if (gps.location.isValid()) {
+	            // isValid checks for the complete GPRMC frame.
+                    sprintf(gps_data, ",%s,%s,%s,%s,%s,%s,%s,%s", gps.location.lat(),
+                                                                  gps.location.lng(),
+                                                                  gps.date.day(),
+                                                                  gps.date.month(),
+                                                                  gps.date.year(),
+                                                                  gps.time.hour(),
+                                                                  gps.time.minute(),
+                                                                  gps.time.second());
+	            break;
+                }
+            }
+        }
+    }
+    Serial.print("GPS data: ");
+    Serial.println(gps_data);
+    return gps_data;
 }
 
 
-String BMP() //BMP180 data gathering. IC out of production, would be wise to replace.
-{
-  char status;
-  const char coma = ',';
-  String zz;
-  double T, P, a;
-  //,Temperatura
-  status = pressure.startTemperature();
-  if (status != 0)
-  {
-    delay(status);
-    status = pressure.getTemperature(T);
-  }
-  if (status != 0)
-  {
-    zz = coma;
-    zz += T;
-
-    //, Presion, Altura_BMP
-    status = pressure.startPressure(3);
-  }
-  else
-  {
-    zz = coma;
-  }
-
-  if (status != 0)
-  {
-    delay(status);
-
-    status = pressure.getPressure(P, T);
-    if (status != 0)
-    {
-      const int P0 = 1013;
-      zz += coma;
-      zz += P;
-
-      a = pressure.altitude(P, P0);
-      zz += coma;
-      zz += a;
+String BMP() {
+    //BMP180 data gathering. IC out of production, would be wise to replace.
+    char status;
+    const char coma = ',';
+    String zz;
+    double T, P, a;
+    //,Temperatura
+    status = pressure.startTemperature();
+    if (status != 0) {
+        delay(status);
+        status = pressure.getTemperature(T);
     }
-  }
-  else
-  {
-    zz += coma;
-    zz += coma;
-  }
-  return zz;
+    if (status != 0) {
+        zz = coma;
+        zz += T;
+        //, Presion, Altura_BMP
+        status = pressure.startPressure(3);
+    }
+    else {
+        zz = coma;
+    }
+    if (status != 0) {
+        delay(status);
+        status = pressure.getPressure(P, T);
+        if (status != 0) {
+            const int P0 = 1013;
+            zz += coma;
+            zz += P;
+
+            a = pressure.altitude(P, P0);
+            zz += coma;
+            zz += a;
+        }
+    }
+    else {
+        zz += coma;
+        zz += coma;
+    }
+    return zz;
 }
 
-int read_ADC(int channel) { //ADC SPI interface
+int read_ADC(int channel) {
+    //ADC SPI interface
+    int adcValue = 0;
+    const int byte8 = 0x06; //setup byte
+    int byte16 = (channel - 1) << 14; //bitshifted channel for second block.
 
-  int adcValue = 0;
-  const int byte8 = 0x06; //setup byte
-  int byte16 = (channel - 1) << 14; //bitshifted channel for second block.
+    digitalWrite(CS_ADC, LOW); //select MCP3204
+    SPI.transfer(byte8);
+    adcValue = SPI.transfer16(byte16) & 0x0FFF; //ADC sample bitmasking.
 
-  digitalWrite(CS_ADC, LOW); //select MCP3204
-  SPI.transfer(byte8);
-  adcValue = SPI.transfer16(byte16) & 0x0FFF; //ADC sample bitmasking.
-
-  digitalWrite(CS_ADC, HIGH); //turn off device
-  return adcValue;
+    digitalWrite(CS_ADC, HIGH); //turn off device
+    return adcValue;
 }
