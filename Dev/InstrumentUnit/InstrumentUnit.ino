9a1c92ed7ec727f0f58037928c029fce09738854
Benjamin Santelices <benjasantelices@hotmail.com>
2022-09-26 16:11:53 -0300

    Fixed commit error

--- Dev/InstrumentUnit/InstrumentUnit.ino
+++ Dev/InstrumentUnit/InstrumentUnit.ino
@@ -6,11 +6,11 @@
 //  /_/  /_/\__,_/_/   \__, /\__,_/_/  /_/\__/\__,_/     /_____/\____/\____/\____/_/  |_/____/_/ |_|  
 //                    /____/                                                                          
 // ------------------------------------------------------------------------------------------------------------------
-// Second Generation Low-Cost Automatic Sun Photometer for scale-oriented measurements of Aerosol Optical Depth (AOD)
+// Fifth Generation Low-Cost Automatic Sun Photometer for scale-oriented measurements of Aerosol Optical Depth (AOD)
 // Space and Planetary Exploration Laboratory, Faculty of Physical and Matemathical Sciences, University of Chile
 // ------------------------------------------------------------------------------------------------------------------
 // First generation developement by Cristobal Garrido and MCI Electronics
-// Second generation developement by Benjamín Santelices, Vicente Aitken, and José Ferrada
+// Second generation developement by Benjamín Santelices, Vicente Aitken, José Ferrada, and Matías VIdal
 // ------------------------------------------------------------------------------------------------------------------
 // INSTRUMENT.INO > Photometer Instrument Unit
 // Firmware for Instrument Unit - Arduino Uno
@@ -93,7 +93,7 @@ String data() //Sensor data processing and collation.
   const char coma = ','; //WARN: is this necessary?
   int readvalue;
   int data1, data2, data3, data4;
-  String zz;
+  char zz[30];
   unsigned long tiempo;
   tiempo = millis(); //WARN: why?
   data1 = 0;
@@ -139,7 +139,7 @@ String data() //Sensor data processing and collation.
 
 String GPS() //GPS data parsing and collation, hugely inneficient. To be replaced by straight NMEA communication.
 {
-  String gps_data = "Invalid data";
+  char gps_data[] = "Invalid data";
   unsigned long tiempo = millis(); //El tiempo de inicio para marcar
 
   while (millis() < tiempo + 30000) {
@@ -147,10 +147,6 @@ String GPS() //GPS data parsing and collation, hugely inneficient. To be replace
       if (gps.encode(ss.read())) {
         if (gps.location.isValid()) {
 	  // isValid checks for the complete GPRMC frame.
-          latlong = true;
-          diamesagno = true;
-          hrminseg = true;
-          alt = true;
 	  sprintf(gps_data, ",%s,%s,%s,%s,%s,%s,%s,%s", gps.location.lat(),
                                                         gps.location.lng(),
                                                         gps.date.day(),
