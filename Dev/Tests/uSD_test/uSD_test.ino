76fee6b35a05ec123aaaf477f5b8061552a05c0f
matvidal <matias.vidal.v@gmail.com>
2022-12-26 19:54:52 -0300

    Minor changes

--- Dev/Tests/uSD_test/uSD_test.ino
+++ Dev/Tests/uSD_test/uSD_test.ino
@@ -24,6 +24,7 @@
 #define CS_ADC 4         // ADC chip select.
 #define CS_SD 10         //SD chip select. Matches hardware SPI bus implementation on 328P.
 
+char filename[20];
 static const PROGMEM uint32_t GPSBaud = 9600; // GPS software UART speed. To be hard-coded, as it does not change.
 struct instrumentStructure {
     int led1 = 0;
@@ -46,59 +47,50 @@ struct instrumentStructure {
 
 void setup() {
     delay(1000);
-    struct instrumentStructure instrumentData;
+    struct instrumentStructure instrumentData[3];
     pinMode(CS_ADC, OUTPUT); // pinMode!!!
     digitalWrite(CS_ADC, HIGH); //turn off device
     Serial.begin(115200);
     Serial.println(F("Testing the structure..."));
-    for (int i=0; i < 10; i++) {
-        SPI.begin();
-        data(&instrumentData); //ADC data
-        SPI.end();
-        delay(100);
-
-        GPS(&instrumentData); //GPS data
-        delay(100);
-
-        BMP(&instrumentData); //BMP180 data
-        delay(100);
-        Serial.println(data2csv(&instrumentData));
+    for (int i=0; i < 5; i++) {
+        measurement(&instrumentData[0]);
+        data2csv(&instrumentData[0]);
+        measurement(&instrumentData[1]);
+        data2csv(&instrumentData[1]);
+        measurement(&instrumentData[2]);
+        data2csv(&instrumentData[2]);
     }
     Serial.println(F("Done."));
-
-    SPI.begin();
-    Serial.print(F("Initiating the uSD card..."));
-    SD.begin(CS_SD); //SD init
-    Serial.println(F("           Done."));
-    delay(100);
-
-    //---DATA STORAGE---
-    File dataFile = SD.open("Data.txt", FILE_WRITE);
-    if (dataFile) { //check availability
-        dataFile.seek(EOF);
-        dataFile.println(data2csv(&instrumentData));
-        dataFile.close();
-        Serial.println(F("Data saved successfully."));
-    }
-    else {
-        Serial.println(F("Error saving to Data.txt")); //WARN: good idea to implement some sort of debugging here.
-    }
-    delay(100);
-
-    Serial.println(F("\nCurrently saved data:\n"));
-    dataFile = SD.open("Data.txt");
-    if (dataFile) {
-        while (dataFile.available()) {
-            Serial.write(dataFile.read());
-        }
-    }
-    dataFile.close();
+    current_saved_data();
 }
 
 void loop() {//nothing happens here.
 }
 
 //---DATA ACQUISITION FUNCTIONS---
+
+void measurement(struct instrumentStructure *instrumentData) {
+    SPI.begin();
+    Serial.print(F("Measuring sensors..."));
+    data(instrumentData); //ADC data
+    Serial.println(F("          Done."));
+    SPI.end();
+
+    Serial.print(F("Reading the GPS module..."));
+    GPS(instrumentData); //GPS data
+    Serial.println(F("     Done."));
+
+    Serial.print(F("Measuring with the BMP180..."));
+    BMP(instrumentData); //BMP180 data
+    Serial.println(F("  Done."));
+    delay(100);
+
+    snprintf(filename, 20, "000/%d%d%d%d.csv",instrumentData->gps_year - 2000,
+                                              instrumentData->gps_month,
+                                              instrumentData->gps_day,                                                                   
+                                              instrumentData->gps_hour);
+    Serial.println(filename);
+}
 
 void data(struct instrumentStructure *instrumentData) {
     //Sensor data processing and collation.
@@ -156,7 +148,7 @@ void BMP(struct instrumentStructure *instrumentData) {
     instrumentData->bmp_alt = random(0, 550000)*0.01;
 }
 
-String data2csv(struct instrumentStructure *instrumentData) {
+void data2csv(struct instrumentStructure *instrumentData) {
     char data_CSV[110] = {0};
     char lat_str[8], lng_str[8], gps_alt_str[8];
     char temp_str[6], pres_str[7], bmp_alt_str[8];
@@ -181,23 +173,53 @@ String data2csv(struct instrumentStructure *instrumentData) {
     else
         dtostrf(instrumentData->bmp_alt, 6, 2, bmp_alt_str);
 
-    sprintf(data_CSV, "007,%d,%d,%d,%d,%s,%c,%s,%c,%d,%d,%d,%d,%d,%d,%s,%s,%s,%s", instrumentData->led1,
-                                                                                   instrumentData->led2,
-                                                                                   instrumentData->led3,
-                                                                                   instrumentData->led4,
-                                                                                   lat_str,
-                                                                                   'S'-5*(instrumentData->gps_lat > 0),
-                                                                                   lng_str,
-                                                                                   'W'-18*(instrumentData->gps_lng > 0),
-                                                                                   instrumentData->gps_day,
-                                                                                   instrumentData->gps_month,
-                                                                                   instrumentData->gps_year,
-                                                                                   instrumentData->gps_hour,
-                                                                                   instrumentData->gps_minute,
-                                                                                   instrumentData->gps_second,
-                                                                                   gps_alt_str,
-                                                                                   temp_str,
-                                                                                   pres_str,
-                                                                                   bmp_alt_str);
-    return data_CSV;
+    sprintf(data_CSV,
+            "000,%d,%d,%d,%d,%s,%c,%s,%c,%d,%d,%d,%d,%d,%d,%s,%s,%s,%s",
+            instrumentData->led1,
+            instrumentData->led2,
+            instrumentData->led3,
+            instrumentData->led4,
+            lat_str,
+            'S'-5*(instrumentData->gps_lat > 0),
+            lng_str,
+            'W'-18*(instrumentData->gps_lng > 0),
+            instrumentData->gps_day,
+            instrumentData->gps_month,
+            instrumentData->gps_year,
+            instrumentData->gps_hour,
+            instrumentData->gps_minute,
+            instrumentData->gps_second,
+            gps_alt_str,
+            temp_str,
+            pres_str,
+            bmp_alt_str);
+    SPI.begin();
+    Serial.print(F("Initiating the uSD card..."));
+    SD.begin(CS_SD); //SD init
+    Serial.println(F("    Done."));
+    delay(100);
+
+    //---DATA STORAGE---
+    File dataFile = SD.open((String) filename, FILE_WRITE);
+    if (dataFile) { //check availability
+        dataFile.seek(EOF);
+        dataFile.println(data_CSV);
+        Serial.println(F("Data saved successfully."));
+    }
+    else {
+        Serial.print(F("Error saving to "));
+        Serial.println(filename);
+    }
+    dataFile.close();
+}
+
+void current_saved_data() {
+    Serial.println(F("\nCurrently saved data:\n"));
+    File dataFile = SD.open(filename);
+    if (dataFile) {
+        while (dataFile.available()) {
+            Serial.write(dataFile.read());
+        }
+    }
+    dataFile.close();
 }
